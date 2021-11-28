df8cfb02f31f6aeeb3bd6a3468df09131628e1f7
Hieu Pham <mrhieuapple@gmail.com>
2021-11-28 17:55:48 +0700

    Implement front-end

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Head from 'next/head';
+import { Container } from "semantic-ui-react";
+
+export default props => {
+  return (
+    <Container style={{ marginTop: '10px' }}>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
+      </Head>
+      <Header />
+      {props.children}
+    </Container>
+  );
+};
---
+++ pages/campaigns/new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+
+class NewCampaign extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>New Campaign</h1>
+      </Layout>
+    );
+  }
+}
+
+export default NewCampaign;
