c57d8590c5477ef389e38d64093dae5a50e555ea
CrabBucket <tom@mcratracing.com>
2022-01-25 17:27:39 -0800

    Added Login UI to the index header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 import { Link } from '../routes';
-
+import Loginheader from './LoginHeader';
 export default () => {
   return (
     <Menu>
       <Link route="/">
         <a className="item">DeCharity</a>
       </Link>
-
+      
       <Menu.Menu position="right">
+        
+        <Loginheader></Loginheader>
         <Link route="/">
           <a className="item">Campaigns</a>
         </Link>
