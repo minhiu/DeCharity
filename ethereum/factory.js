fbbe2335a6714be43d6470d6529daf636cf0cac0
Hieu Pham <mrhieuapple@gmail.com>
2022-01-27 18:27:46 -0800

    Updated contract address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x1ddaC4A66844e19d4185d16B61fCEfD0eDB8e24C'
+  '0x4aA98317f9B49BF9ad3821C2e303d196f581B05c'
 );
 
 export default instance;
