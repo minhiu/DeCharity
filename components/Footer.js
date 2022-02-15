d9574773ca971052cf1cc319c93c592719a2b4d8
jichiangEX <60938970+jichiangEX@users.noreply.github.com>
2022-02-15 10:59:03 -0800

    Templates for About and Contact
    added About.js and Contact.js

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <footer class="py-5 bg-dark fixed-bottom">
+                <div class="container">
+                    <p class="m-0 text-center text-white">
+                        Copyright &copy; DeCharity 2021-2022
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+}
---
+++ components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+function Navigation() {
+    return (
+        <div className="navigation">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container">
+                    <NavLink className="navbar-brand" to="/">
+                        DeCharity Homepage
+                    </NavLink>
+                    <div>
+                        <u1 className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/">
+                                    Home
+                                    <span className="sr-only">(current)</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to = "/about">
+                                    About
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/contact">
+                                    Contact
+                                </NavLink>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to = "/blog">
+                                        Blog
+                                    </NavLink>
+                                </li>
+                            </li>
+                        </u1>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
