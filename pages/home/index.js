49dc9bf82b468659a6233507bb5145b8859a77f7
jichiangEX <60938970+jichiangEX@users.noreply.github.com>
2022-02-15 11:15:32 -0800

    Create index.js
    added home folder in the pages directory and added index.js

---
+++ pages/home/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+    Navbar,
+    Footer,
+    Home,
+    About,
+    Contact
+} from "./components";
+
+ReactDOM.render(
+    <Router>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" elements={<Contact />} />
+        </Routes>
+        <Footer />
+    </Router>
+
+    document.getElementById("root")
+);
+
+serviceWorker.unregister();
