216598b2b818ce4330bb9c69d8a0d1e1ec9961b5
justinnguyen22 <80489273+justinnguyen22@users.noreply.github.com>
2022-02-21 16:45:59 -0800

    User set data
    Allows users to set data and save to db.  Curently, box is difficult to type in, researching different imports to fix this.

--- components/UserData.js
+++ components/UserData.js
@@ -4,24 +4,33 @@ import { useState } from "react";
 import { Divider, Input } from "semantic-ui-react";
 
 export const SignUp = () => {
-    const { isAuthenticated, user } = useMoralis();
+    const { isAuthenticated, user , setUserData} = useMoralis();
 
     const [username, setUsername ] = useState();
     const [ email, setEmail ] = useState();
     const [ password, setPassword ] = useState('');
     const [birthdate, setBirthdate ] = useState();
 
+    const handleSave = () => {
+      setUserData({
+        username,
+        email,
+        birthdate,
+        password: password === "" ? undefined : password
+      })
+    } 
+
     if(isAuthenticated) {
       return (
         <div>
           <text>Username  </text>
-          <Input value={user.attributes.username} />
+          <Input value={user.attributes.username} onChange={(event) => setUsername(event.currentTarget.value)}/>
           <Divider></Divider>
           <text>Email  </text>
-          <Input value={user.attributes.email} />
+          <Input value={user.attributes.email} onChange={(event) => setEmail(event.currentTarget.value)}/>
           <Divider></Divider>
           <text>Birth Date  </text>
-          <Input value={user.attributes.birthdate} />
+          <Input value={user.attributes.birthdate} onChange={(event) => setBirthdate(event.currentTarget.value)}/>
           <Divider></Divider>
           <text>EthAddress  </text>
           <Input value={user.attributes.ethAddress} />
@@ -29,6 +38,7 @@ export const SignUp = () => {
           <text>Password</text>
           <Input value={password} />
           <Divider></Divider>
+          <button onClick={handleSave}>Submit Changes</button>
         </div>
       );
     } else {
