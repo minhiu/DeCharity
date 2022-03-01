f57017348b22583dee50de8be5e6951335e380b1
jichiangEX <60938970+jichiangEX@users.noreply.github.com>
2022-03-01 15:39:10 -0800

    webpack addition
    added in the start of webpack to add local images into react

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import Layout from '../components/Layout';
-import logo from './decharitylogo.png';
+import logo from '../public/images/decharitylogo.png';
 
 export default function Home() {
     return (
         <Layout>
             <div>
                 <h1>DeCharity</h1>
-                <img src={logo} alt='logo' />
+                <div>
+                    <img src={logo} alt='logo' /> 
+                </div>
                 <div>
                     <br></br>
                     <p1>
