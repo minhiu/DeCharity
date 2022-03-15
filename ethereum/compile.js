7e29b6c92ff8757085a1e7ce1fed5e5ab3a05737
CrabBucket <tom@mcratracing.com>
2022-03-15 14:30:21 -0700

    revert

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -5,12 +5,45 @@ const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
+
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
-const source = fs.readFileSync(campaignPath, 'utf-8');
-const output = solc.compile(source, 1).contracts;
+const ierc20Path = path.resolve(__dirname, 'contracts', 'IERC20.sol');
 
+var input = {
+  language: 'Solidity',
+  sources: {
+    'Campaign.sol': {
+      content: fs.readFileSync(campaignPath, 'utf-8')
+    },
+    'IERC20.sol': {
+      content: fs.readFileSync(ierc20Path, 'utf-8')
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+};
+
+function findImports(path) {
+  if (path === 'lib.sol')
+    return {
+      contents:
+        'library L { function f() internal returns (uint) { return 7; } }'
+    };
+  else return { error: 'File not found' };
+}
+
+// New syntax (supported from 0.5.12, mandatory from 0.6.0)
+var output = JSON.parse(
+  solc.compile(JSON.stringify(input), { import: findImports })
+);
+
+// Export output to build path
 fs.ensureDirSync(buildPath);
-
 for (let contract in output) {
   fs.outputJSONSync(
     path.resolve(buildPath, contract.replace(':', '') + '.json'),
