2bdb51b8227fde1ae9030ff35e1f7b9d9d5d6543
justinnguyen22 <80489273+justinnguyen22@users.noreply.github.com>
2022-04-24 16:31:04 -0700

    Map campaigns to profile page
    Mapped campaigns into card for profile page. Need to style next

--- pages/profile.js
+++ pages/profile.js
@@ -5,12 +5,33 @@ import Header from "../components/Header";
 import factory from "../ethereum/factory";
 
 class ProfilePage extends Component {
+
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        // console.log(campaigns);
+        
+        return { campaigns };
+      }
+
+    
     render() {
         const user = {
             name: 'Bart Veneman',
             username: 'bartveneman',
             gravatar: 'https://gravatar.com/avatar/56d18ba2b0bf189436499b0f215b5e29?s=100'
         }
+
+        const cList = this.props.campaigns.map((item) => {
+            return (
+                <Card>
+                    <Card.Header>
+                        {item}
+                    </Card.Header>
+                </Card>
+            );
+        })
+    //    return <div> {this.props.campaigns} </div>
+        
         return (
             <MoralisProvider 
         appId="mtI7RNIJyg9YQ9KlY0lxRWT07i948n2P18xGY9lJ"
@@ -30,38 +51,33 @@ class ProfilePage extends Component {
                         </div>
                     </div>
 
-                    {/* <div className="profile-projects">
-                        <h2> #{user.name}'s Projects </h2>
+                    <div className="profile-projects">
+                        <h2> {user.name}'s Campaigns </h2>
                         <ol className="cards">
-                            <mapProjects></mapProjects>
+                            {cList}
                         </ol>
-                    </div> */}
+                    </div>
                 </div>
             </MoralisProvider>
             )
     }
 
-    static async getInitialProps() {
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns);
-        return { campaigns };
-      }
+  
+    // renderCampaigns() {
+    //     const items = this.props.campaigns.map((address) => {
+    //       return {
+    //         header: address,
+    //         description: (
+    //           <Link route={`/campaigns/${address}`}>
+    //             <a>View Campaign</a>
+    //           </Link>
+    //         ),
+    //         fluid: true,
+    //       };
+    //     });
     
-    renderCampaigns() {
-        const items = this.props.campaigns.map((address) => {
-          return {
-            header: address,
-            description: (
-              <Link route={`/campaigns/${address}`}>
-                <a>View Campaign</a>
-              </Link>
-            ),
-            fluid: true,
-          };
-        });
-    
-        return <Card.Group items={items} />;
-    }
+    //     return <Card.Group items={items} />;
+    // }
    
 }
 
